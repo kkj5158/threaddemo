a214bfb0754a140a61add28ee2a3edf324ad40d2
jjk5158 <jjk5158@naver.com>
2025-04-22 12:44:57 +0900

    Initial commit

---
+++ src/test/java/linge/threaddemo/ThreaddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package linge.threaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreaddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
